02f14cb0ffadf41bac319080f35a6a8027b409ee
Kyle Henderson <ona@lunch.run>
2023-05-13 20:21:28 -0500

    Got it working the tray menu

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::time::SystemTime;
-
-use tauri::{Manager, Menu, SystemTray, SystemTrayEvent, SystemTrayMenu};
-use tauri_plugin_positioner::{Position, WindowExt};
+use anyhow::Result;
+use tauri::{CustomMenuItem, Manager, SystemTray, SystemTrayMenu, SystemTrayMenuItem};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -15,42 +13,58 @@ fn greet(name: &str) -> String {
 #[tauri::command]
 async fn open_app(path: &str) -> Result<(), String> {
     let path = std::path::Path::new(path);
-    let command = format!("open -a {}", path.display());
-    println!("Running command: {}", command);
-    let mut cmd = std::process::Command::new(command);
-    cmd.spawn().map_err(|e| e.to_string())?;
+    std::process::Command::new("open")
+        .arg("-a")
+        .arg(path.as_os_str())
+        .spawn()
+        .map_err(|e| e.to_string())?;
 
     Ok(())
 }
 
 fn main() {
-    let system_tray_menu = SystemTrayMenu::new();
+    let system_tray_menu = SystemTrayMenu::new()
+        .add_item(CustomMenuItem::new("Version", "Version 0.0.0".to_string()).disabled())
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(CustomMenuItem::new(
+            "Launch on Login",
+            "Launch on Login".to_string(),
+        ))
+        .add_item(CustomMenuItem::new(
+            "Show in Dock",
+            "Show in Dock".to_string(),
+        ))
+        .add_item(CustomMenuItem::new(
+            "Edit Shortcuts",
+            "Edit Shortcuts".to_string(),
+        ))
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(CustomMenuItem::new("Github", "Github".to_string()))
+        .add_item(CustomMenuItem::new("Quit", "Quit".to_string()));
     tauri::Builder::default()
-        .plugin(tauri_plugin_positioner::init())
         .system_tray(SystemTray::new().with_menu(system_tray_menu))
-        .on_system_tray_event(|app, event| {
-            tauri_plugin_positioner::on_tray_event(app, &event);
-
-            match event {
-                SystemTrayEvent::LeftClick {
-                    position: _,
-                    size: _,
-                    ..
-                } => {
+        .on_system_tray_event(|app, event| match event {
+            tauri::SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+                "Edit Shortcuts" => {
+                    app.show().expect("failed to show app");
                     let window = app.get_window("main").unwrap();
-                    // toggle application window
-                    let _ = window.move_window(Position::TrayCenter);
-                    if window.is_visible().unwrap() {
-                        window.hide().unwrap();
-                    } else {
-                        window.show().unwrap();
-                        window.set_focus().unwrap();
-                    }
+                    window.show().unwrap();
                 }
+                "Quit" => app.exit(0),
                 _ => {}
-            }
+            },
+            _ => {}
         })
         .invoke_handler(tauri::generate_handler![greet, open_app])
+        .on_window_event(|event| match event.event() {
+            tauri::WindowEvent::CloseRequested { api, .. } => {
+                api.prevent_close();
+                event.window().hide().unwrap();
+            }
+            _ => {
+                println!("Window event: {:?}", event.event())
+            }
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
